bfd841355ba07e13f099c5219a420e8aeeb71480
VolodymyrVelch <velch83@gmail.com>
2022-07-23 14:51:13 +0300

    js-hw-07-task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const list = galleryItems.map(
+  g =>
+    `
+  <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src=${g.preview}
+        data-source=${g.original}
+        alt=${g.description}
+      />
+    </a>
+  </div>
+  `
+);
+galleryList.insertAdjacentHTML('afterbegin', list.join(''));
+
+galleryList.addEventListener('click', itemsShow);
+
+function itemsShow(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const soursData = e.target.getAttribute('data-source');
+
+  // // Через querySelector - null
+  // const soursData = e.target.querySelector('data-source');
+  // console.log(srcData);
+
+  const instance = basicLightbox.create(`
+      <img src=${soursData} >
+  `);
+
+  instance.show();
+}
